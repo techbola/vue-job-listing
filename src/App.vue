080db3ec749a360280d33a8cfcb917f9514bbfec
Adebola <adebola@poptin.com>
2024-07-04 13:23:42 +0100

    NavBar component, Hero component, Card component, JobListings component, JobListing component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <NavBar />
   <Hero />
   <HomeCards />
+  <JobListings :limit="3" showButton />
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 import Hero from '@/components/Hero.vue'
 import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" class="bg-white rounded-xl shadow-md relative" />
+      </div>
+    </div>
+  </section>
+
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="jobs.html"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
+  </section>
+</template>
+
+<script setup>
+import jobData from '@/jobs.json'
+import { ref } from 'vue';
+import JobListing from '@/components/JobListing.vue';
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const jobs = ref(jobData.jobs);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
