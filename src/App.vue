4a229b413dc6b6687030c4e1f94ea15898683783
Adebola <adebola@poptin.com>
2024-07-04 12:47:28 +0100

    project setup, install and setup tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavBar />
+  <Hero />
+  <HomeCards />
+</template>
+
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+</script>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  bg: {
+    type: String,
+    default: 'bg-gray-100'
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
