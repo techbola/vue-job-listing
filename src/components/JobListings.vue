288e52ef6bc375703302d855e58e9904b5eba3fe
Adebola <adebola@poptin.com>
2024-07-04 14:48:24 +0100

    used reactive on JobListings page, installed and used vue-spinner

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,13 +4,16 @@
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" class="bg-white rounded-xl shadow-md relative" />
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" class="bg-white rounded-xl shadow-md relative" />
       </div>
     </div>
   </section>
 
-  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+  <section v-if="state.jobs.length > 0 && showButton" class="m-auto max-w-lg my-10 px-6">
     <router-link
       :to="{ name: 'Jobs' }"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
@@ -21,8 +24,9 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { reactive, onMounted } from 'vue';
 import JobListing from '@/components/JobListing.vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 
 defineProps({
@@ -33,14 +37,20 @@ defineProps({
   }
 });
 
-const jobs = ref([]);
+const state = reactive({
+  jobs: [],
+  isLoading: false
+});
 
 onMounted(async () => {
   try {
+    state.isLoading = true
     const { data } = await axios.get('http://localhost:8000/jobs');
-    jobs.value = data
+    state.jobs = data
   } catch (error) {
     console.error('Error fetching jobs', error)
+  } finally {
+    state.isLoading = false
   }
 })
 </script>
